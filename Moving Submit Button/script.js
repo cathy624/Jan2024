4d6ea853c2e0a1834b6f29c8633cb90a4d694487
cathy624 <120194359+cathy624@users.noreply.github.com>
2024-03-15 17:33:20 +0800

    new

---
+++ Moving Submit Button/script.js
@@ -0,0 +1,17 @@
+usernameRef.addEventListener('input',  () => {
+    if (!isUsernameValid()) {
+        message_alert.style.visibility = 'hidden';
+        usernameRef.style.cssText = 'border-color: red; background-color: #FFB6C1';
+    } else {
+        usernameRef.style.cssText = 'border-color: green; background-color: #90EE90';
+    }
+});
+
+passwordRef.addEventListener('input',  () => {
+    if (!isPasswordValid()) {
+        message_alert.style.visibility = 'hidden';
+        passwordRef.style.cssText = 'border-color: red; background-color: #FFB6C1';
+    } else {
+        passwordRef.style.cssText = 'border-color: green; background-color: #90EE90';
+    }
+});
